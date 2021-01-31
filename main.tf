6db8d19c7fc3ab0de56e5d191dfd8e94c0000b40
Tomaž Karer <tomazkarer@gmail.com>
2021-01-31 22:56:04 +0100

    naredi strežnik

---
+++ main.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers {
+    hcloud = {
+      source = "hetznercloud/hcloud"
+      version = "1.24.0"
+    }
+  }
+}
+
+variable "hcloud_token" {}
+
+provider "hcloud" {
+  token = var.hcloud_token
+}
+
+resource "hcloud_network" "network" {
+  name = "mars_network"
+  ip_range = "10.0.0.0/16"
+}
+
+resource "hcloud_server" "server" {
+  name = "mars"
+  server_type = "cx11"
+  image = "debian-10"
+  location = "nbg1"
+  ssh_keys = ["${hcloud_ssh_key.default.id}"]
+
+#  network { 
+#    network_id = hcloud_network.network.id
+#    ip = "10.0.1.5"
+#  }
+}
+
+resource "hcloud_ssh_key" "default" {
+  name = "mars_ssh"
+  public_key = file("./.ssh/id_rsa.pub")
+}
